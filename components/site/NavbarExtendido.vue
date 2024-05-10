88927c1be40cbba4e558e156522b8ee1ae4d5b50
Lucas Ferrero <lucasdavidferrero@gmail.com>
2024-05-10 12:26:24 -0300

    agrego botones de acceso rápido al listado de rubros.

--- components/site/NavbarExtendido.vue
+++ components/site/NavbarExtendido.vue
@@ -3,7 +3,12 @@ import { ref } from 'vue'
 import { SfListItem, SfButton, SfIconMenu, SfDropdown, useDisclosure, SfIconChevronRight } from '@storefront-ui/vue'
 
 const { isOpen, toggle } = useDisclosure();
-
+const rubrosDeAccesoRapido = [
+  { aik_re2_codigo: '00002', aik_re2_descri: 'CALEFACCION' },
+  { aik_re2_codigo: '00008', aik_re2_descri: 'SMARTWATCH' },
+  { aik_re2_codigo: '00011', aik_re2_descri: 'COCINAS Y HORNOS' },
+  { aik_re2_codigo: '00013', aik_re2_descri: 'TERMOTANQUE Y CALEFON'}
+]
 const listadoCategorias = [
   { aik_re1_codigo: '00001', aik_re1_descri: 'CLIMATIZACION' },
   { aik_re1_codigo: '00002', aik_re1_descri: 'TV AUDIO VIDEO' },
@@ -18,6 +23,10 @@ const listadoCategorias = [
   { aik_re1_codigo: '00011', aik_re1_descri: 'RODADOS'},
   { aik_re1_codigo: '00012', aik_re1_descri: 'NIÑOS'},
 ]
+
+function normalizarDescripcionesJerarquia (descripcion: string) {
+  return descripcion[0] + descripcion.slice(1).toLowerCase()
+}
 </script>
 
 <template>
@@ -25,7 +34,7 @@ const listadoCategorias = [
     <div>
       <!-- Desktop dropdown -->
       <nav ref="floatingRef">
-        <ul class="hidden md:flex px-6 py-2 bg-primary-800 border-b border-b-neutral-200 border-b-solid">
+        <ul class="hidden md:flex px-6 py-2 border-b border-b-neutral-200 border-b-solid">
           <li>
             <SfDropdown v-model="isOpen" placement="bottom-start">
               <template #trigger>
@@ -37,7 +46,7 @@ const listadoCategorias = [
                     @click="toggle"
                 >
                   <SfIconMenu
-                      class="text-neutral-500 group-hover:text-neutral-700 group-active:text-neutral-900 mt-0.5"></SfIconMenu>
+                      class="text-neutral-500 group-hover:text-neutral-700 group-active:text-neutral-900 "></SfIconMenu>
                   <span>Categorías</span>
                 </SfButton>
               </template>
@@ -45,7 +54,7 @@ const listadoCategorias = [
                 <!-- Categorías -->
                 <div class="max-w-sm">
                   <SfListItem v-for="categoriaItem in listadoCategorias">
-                    <span class="break-words">{{ categoriaItem.aik_re1_descri }}</span>
+                    <span class="break-words">{{ normalizarDescripcionesJerarquia(categoriaItem.aik_re1_descri) }}</span>
                     <template #suffix><SfIconChevronRight /></template>
                   </SfListItem>
                 </div>
@@ -54,47 +63,19 @@ const listadoCategorias = [
                 <div></div>
               </ul>
             </SfDropdown>
-            <!--<div
-                v-if="isOpen && activeNode.length === 1 && activeNode[0] === menuNode.key"
-                :key="activeMenu.key"
-                ref="megaMenuRef"
-                :style="style"
-                class="hidden md:grid gap-x-6 grid-cols-4 bg-white shadow-lg p-6 left-0 right-0 outline-none"
-                tabindex="0"
-                @mouseleave="close()"
-                @keydown.esc="focusTrigger(index)"
+          </li>
+          <li v-for="rubro in rubrosDeAccesoRapido">
+            <SfButton
+                ref="triggerRefs"
+                variant="tertiary"
+                class="group mr-2 !text-neutral-900 hover:!bg-neutral-200 hover:!text-neutral-700 active:!bg-neutral-300 active:!text-neutral-900"
+                @click=""
             >
-              <template v-for="node in activeMenu.children" :key="node.key">
-                <template v-if="node.isLeaf">
-                  <SfListItem tag="a" size="sm" :href="node.value.link" class="typography-text-sm mb-2">
-                    {{ node.value.label }}
-                  </SfListItem>
-                  <div class="col-start-2 col-end-5" />
-                </template>
-                <div v-else>
-                  <p
-                      class="typography-text-base font-medium text-neutral-900 whitespace-nowrap px-4 py-1.5 border-b border-b-neutral-200 border-b-solid"
-                  >
-                    {{ node.value.label }}
-                  </p>
-                  <ul class="mt-2">
-                    <li v-for="child in node.children" :key="child.key">
-                      <SfListItem tag="a" size="sm" :href="child.value.link" class="typography-text-sm py-1.5">
-                        {{ child.value.label }}
-                      </SfListItem>
-                    </li>
-                  </ul>
-                </div>
-              </template>
-              <div
-                  class="flex flex-col items-center justify-center overflow-hidden rounded-md bg-neutral-100 border-neutral-300 grow"
-              >
-                <img :src="bannerNode.value.banner" :alt="bannerNode.value.bannerTitle" class="object-contain" />
-                <p class="px-4 mt-4 mb-4 font-medium text-center typography-text-base">
-                  {{ bannerNode.value.bannerTitle }}
-                </p>
-              </div>
-            </div>-->
+              <span>{{ normalizarDescripcionesJerarquia(rubro.aik_re2_descri)}}</span>
+              <!--<SfIconChevronRight
+                  class="rotate-90 text-neutral-500 group-hover:text-neutral-700 group-active:text-neutral-900"
+              />-->
+            </SfButton>
           </li>
         </ul>
       </nav>
